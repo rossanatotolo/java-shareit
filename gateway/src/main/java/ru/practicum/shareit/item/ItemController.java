15c5570a05cd40763937a63e284712bef8a242e2
Rossana <rossanatotolo@yandex.ru>
2024-11-03 16:03:47 +0300

    fix bug

--- gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
+++ gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/items")
